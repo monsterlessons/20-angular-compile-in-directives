94af95449a20a11c7f2fbca2b3b4410aab3d798b
Alexander Kochergin <ejiqpep@gmail.com>
2015-06-21 23:47:22 +0300

    Added lesson

---
+++ main.js
@@ -0,0 +1,17 @@
+var app = angular.module('app', []);
+
+app.directive('uiSource', function () {
+  return {
+    compile: function (elem) {
+      var escape = function (content) {
+        return content.replace(/\</g, '&lt;')
+                      .replace(/\>/g, '&gt;');
+      };
+      var pre = angular.element('<pre class="prettyprint"></pre');
+      var pretty = prettyPrintOne(escape(elem.html()));
+      console.log(pretty);
+      pre.append(pretty);
+      elem.replaceWith(pre);
+    }
+  };
+});
